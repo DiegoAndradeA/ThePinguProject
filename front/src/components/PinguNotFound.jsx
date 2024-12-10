923246db569c274028da2d23fb4f9f2eea1065fd
gfernandezr <gfernandez.r@uc.cl>
2024-12-09 23:38:58 -0300

    pepo14: added not found page

--- front/src/components/PinguNotFound.jsx
+++ front/src/components/PinguNotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function PinguNotFound() {
-  return <div>PinguNotFound</div>;
-}
+import sadPingu from "../assets/notfoundimg.gif";
 
-export default PinguNotFound;
+export default function PokeNotFound() {
+  document.title = "Oops! - PiplupPedia";
+  return (
+    <div>
+      <b className="text-blue-600 text-5xl sm:text-6xl">¡Oops!</b>
+      <img src={sadPingu} className="mx-auto mt-10 mb-5 rounded-lg w-[300px]" />
+      <p className="text-blue-600 text-3xl mb-5">
+        Al parecer, la pagina que buscas no existe.
+      </p>
+    </div>
+  );
+}
